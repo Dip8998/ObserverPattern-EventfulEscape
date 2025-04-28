c093f604575c29000ecfa1458a34ca1891daa82e
Dip Patel <pateldeep2332@gmail.com>
2025-04-28 20:52:22 +0530

    Created an Event Service

---
+++ Assets/Scripts/Service/EventService.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EventService 
+{
+    private static EventService instance;
+    public static EventService Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new EventService();
+            }
+            return instance;
+        }
+    }
+
+    public EventController OnLightSwitchToggled;
+
+    public EventService()
+    {
+        OnLightSwitchToggled = new EventController();
+    }
+}
