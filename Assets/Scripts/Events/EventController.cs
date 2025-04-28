d9a00da5c1b21dcd32829e7403c0a0b775a5dec5
Dip Patel <pateldeep2332@gmail.com>
2025-04-28 20:31:38 +0530

    Created an Event Controller

---
+++ Assets/Scripts/Events/EventController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System;
+
+public class EventController 
+{
+    public Action baseEvent;
+
+    public void AddListner(Action listner) => baseEvent += listner;
+
+    public void RemoveListner(Action listner) => baseEvent -= listner; 
+
+    public void InvokeEvent() => baseEvent?.Invoke();
+}
